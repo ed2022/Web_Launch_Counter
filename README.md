d84199fae8c6f7e699967df28580bc05e5fea59c
Nova <eldanadesalgne@gmail.com>
2023-09-27 17:14:05 -0400

    Updated and placed the css and ejs(html) within the pages including get-post routes

---
+++ README.md
@@ -0,0 +1,36 @@
+# Web_Launch_Counter
+
+Welcome to my website landing page project! This project showcases my skills in using Node.js, MySQL, HTML, and CSS to create a landing page for collecting emails in preparation for an upcoming website launch.
+
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Features](#features)
+- [Requirements](#requirements)
+- [Getting Started](#getting-started)
+- [Project Structure](#project-structure)
+- [Technologies Used](#technologies-used)
+- [Demo](#demo)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Introduction
+
+This project is a website landing page designed to create excitement and anticipation for an upcoming website launch. Visitors to the landing page can subscribe by providing their email addresses, which are stored in a MySQL database. The landing page features a responsive design to ensure a seamless user experience. 
+
+## Features and Technology Used
+
+- **Email Subscription:** Visitors can enter their email addresses to subscribe to updates about the upcoming website launch.
+
+- **MySQL Database:** Collected email addresses are stored securely in a MySQL database.
+
+- **Responsive Design:** The landing page is responsive and optimized for various screen sizes.
+
+- **HTML and CSS:** The landing page is built using HTML and styled using CSS to provide an attractive and user-friendly interface.
+
+- **Node.js Server:** Node.js is used to serve the landing page and handle email subscription requests.
+
+- **Data Generation with Faker:** Fake user data generation powered by the Faker library. This feature allows you to populate your MySQL database with realistic dummy data for testing and development purposes.
+
+- **Dynamic Content with EJS:** The EJS (Embedded JavaScript) template engine is integrated to create dynamic content for your landing page. It simplifies the process of rendering HTML with data from your Node.js server, making it easier to display dynamic information and customize your web pages.
+
--- script.js
+++ script.js
@@ -5,6 +5,13 @@ const { faker } = require('@faker-js/faker');
 // EXPRESS 
 const express = require('express');
 const app = express();
+// EJS 
+app.set("view engine", "ejs"); 
+//BODY PARSER (npm install --save body-parser)
+const bodyParser = require('body-parser'); 
+app.use(bodyParser.urlencoded({extended: true})); 
+//STYLE CONNECTION
+app.use(express.static(__dirname + "/public")); 
 // MYSQL
 const mysql = require('mysql');
 const connection = mysql.createConnection(
@@ -30,26 +37,38 @@ app.get("/", function(req, res){
   connection.query(q, function(error, results){
           if(error) throw error; 
           let count = results[0].count;
-          res.send("We have " + count + " users in our db!");
+          res.render("home", {data : count}); 
   }); 
 }); 
 
-// MYSQL 
-// const q = 'INSERT INTO users (email, created_at) VALUES ?'; 
-// const data = []; 
+//POST ROUTE: this is to help us save and use the input from client in the form. The only way that this page will be triggered is if it is sent to the method. 
+app.post('/register', function(req,res){
+  const person = {email: req.body.email}; 
+  connection.query('INSERT INTO users SET?', person, function(error, results){
+    if(error) throw error; 
+    res.redirect("/"); //takes us back to the original page
+  })
+}); 
 
-// for (let i = 0; i < 500; i++) {
-//     data.push([
-//         faker.internet.email(), 
-//         faker.date.past()
-//     ])
-// }
-// console.log(data); 
-// This is the connection and query. Error will throw when detected by mysql, results = answer. 
-// connection.query(q, [data], function(error, results){
-//     if(error) throw error; 
-//     console.log(results);
-// })
-// // This will save us time to not have to click Ctrl + C  
+
+// {MYSQL - to run the original 500 db- people
+//   const q = 'INSERT INTO users (email, created_at) VALUES ?'; 
+//   const data = []; 
+
+//   for (let i = 0; i < 500; i++) {
+//       data.push([
+//           faker.internet.email(), 
+//           faker.date.past()
+//       ])
+//   }
+//   console.log(data); 
+//   This is the connection and query. Error will throw when detected by mysql, results = answer. 
+//   connection.query(q, [data], function(error, results){
+//       if(error) throw error; 
+//       console.log(results);
+//   })
+// This will save us time to not have to click Ctrl + C  
+//}
+// Then we install ejs : embedded js language : "npm install --save ejs". 
 
 
